d2e6c2f86b927603df930327075ae95dd58c9c88
BALUSANGEM <vishnubalu.sangem@gmail.com>
2023-08-17 12:56:14 +0530

    Creating a skeleton for ButtonPrimitive

--- orbit-design/src/main/java/com/nosort/orbit_design/components/ButtonDefaults.kt
+++ orbit-design/src/main/java/com/nosort/orbit_design/components/ButtonDefaults.kt
@@ -1,4 +1,18 @@
 package com.nosort.orbit_design.components
 
-class ButtonDefaults {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+
+object ButtonDefaults {
+    internal val minWidth = 64.dp
+    internal val minHeight = 32.dp
+
+    internal val horizontalPadding = 16.dp
+    internal val verticalPadding = 14.dp
+
+    internal val ContentPadding = PaddingValues(horizontalPadding, verticalPadding)
+
+    internal val horizontalArrangement = Arrangement.spacedBy(space = 0.dp, Alignment.CenterHorizontally)
 }
--- orbit-design/src/main/java/com/nosort/orbit_design/components/ButtonPrimitive.kt
+++ orbit-design/src/main/java/com/nosort/orbit_design/components/ButtonPrimitive.kt
@@ -1,4 +1,54 @@
 package com.nosort.orbit_design.components
 
-class ButtonPrimitive {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.tooling.preview.Preview
+import com.nosort.orbit_design.components.ButtonDefaults.ContentPadding
+import com.nosort.orbit_design.components.ButtonDefaults.horizontalArrangement
+
+@Composable
+fun ButtonPrimitive(
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.() -> Unit
+) {
+    Box(modifier = modifier.background(Color.Blue)) {
+        Row(
+            modifier = Modifier.defaultMinSize(
+                minWidth = ButtonDefaults.minWidth,
+                minHeight = ButtonDefaults.minHeight
+            ).padding(ContentPadding),
+            horizontalArrangement = horizontalArrangement,
+            verticalAlignment = Alignment.CenterVertically,
+            content = content
+        )
+    }
+}
+
+@Composable
+fun Text(string: AnnotatedString) {
+    BasicText(string)
+}
+
+@Composable
+fun Text(string: String) {
+    BasicText(AnnotatedString(string))
+}
+
+@Preview()
+@Composable
+fun ButtonPrimitivePreview() {
+    ButtonPrimitive {
+        Text("Primitive Button")
+    }
 }
