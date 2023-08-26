de365b0a5743102aae85c14a7b671211dc4fb4e2
BALUSANGEM <vishnubalu.sangem@gmail.com>
2023-08-26 23:15:04 +0530

    Create placeholder for slider components

---
+++ orbit-design/src/main/java/com/nosort/orbit_design/components/slider/RangeSlider.kt
@@ -0,0 +1,9 @@
+package com.nosort.orbit_design.components.slider
+
+import androidx.compose.runtime.Composable
+import com.nosort.orbit_design.components.Text
+
+@Composable
+fun RangeSlider() {
+    Text(string = "Range slider")
+}
---
+++ orbit-design/src/main/java/com/nosort/orbit_design/components/slider/Slider.kt
@@ -0,0 +1,9 @@
+package com.nosort.orbit_design.components.slider
+
+import androidx.compose.runtime.Composable
+import com.nosort.orbit_design.components.Text
+
+@Composable
+fun Slider() {
+    Text(string = "Normal Slider")
+}
